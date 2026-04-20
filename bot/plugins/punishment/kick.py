c2f37a55657c82f7a63adaaf9869c1eda1d6fa9a
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-04-20 21:47:24 +0700

    Create kick.py

---
+++ bot/plugins/punishment/kick.py
@@ -0,0 +1,8 @@
+# plugins/punishment/kick.py
+
+async def kick_user(member, reason=None):
+
+    try:
+        await member.kick(reason=reason or "No reason provided")
+    except Exception as e:
+        print("KICK ERROR:", repr(e))

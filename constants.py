a078f01b0a3753e24bca97d4fec4793404cd74e8
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-05-12 03:33:46 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,11 +1,14 @@
-# constants.py - Lưu tại thư mục gốc
-COLOR_GENERAL = 0x010101   # Đen tuyền
-COLOR_SILENCE = 0x000000   # Đen tuyệt đối
-COLOR_AUDIT = 0x111111     # Xám tối
+# constants.py 
+COLOR_GENERAL = 0x010101   
+COLOR_SILENCE = 0x000000   
+COLOR_AUDIT = 0x111111     
 
-# Cấu hình ngưỡng Anti-Spam
-SPAM_MSG_LIMIT = 5
-SPAM_LINK_LIMIT = 2
-SPAM_MENTION_LIMIT = 5
+# Cấu hình mặc định
+DEFAULT_CONFIG = {
+    "active": True,
+    "check_messages": True,
+    "check_links": True,
+    "check_mentions": True
+}
 
 

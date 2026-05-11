e5d21014269e7d6a89a812dedede0d3beb27c643
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-05-12 04:11:42 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,14 +1,12 @@
-# constants.py 
-COLOR_GENERAL = 0x010101   
-COLOR_SILENCE = 0x000000   
-COLOR_AUDIT = 0x111111     
-
-# Cấu hình mặc định
 DEFAULT_CONFIG = {
     "active": True,
     "check_messages": True,
     "check_links": True,
-    "check_mentions": True
+    "check_mentions": True,
+    
+    # Ngưỡng mặc định nếu DB chưa có
+    "max_mentions": 5,      # Quá 5 tags là vả
+    "max_messages": 7,      # Quá 7 tin nhắn/5s là vả
+    "max_links": 3,         # Quá 3 links là vả
+    "timeout_duration": 2419200 # 28 ngày tính bằng giây
 }
-
-

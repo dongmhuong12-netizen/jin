0a8b2a53746571e09246dfc2c98fd7b8de348df3
dongmhuong12-netizen <dongmhuong12@gmail.com>
2026-05-12 02:59:55 +0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,9 +1,11 @@
-# constants.py
-COLOR_SILENCE = 0x000000  # Đen tuyệt đối cho kênh Khóa mõm
-COLOR_AUDIT = 0x010101    # Đen mờ cho Audit Log và General
-COLOR_SYSTEM = 0x010101   # Màu chủ đạo của Jin
+# constants.py - Lưu tại thư mục gốc
+COLOR_GENERAL = 0x010101   # Đen tuyền
+COLOR_SILENCE = 0x000000   # Đen tuyệt đối
+COLOR_AUDIT = 0x111111     # Xám tối
 
-# Cấu hình mặc định Anti-Spam
-DEFAULT_SPAM_MSG = 5      # 5 tin / 1 giây
-DEFAULT_SPAM_LINK = 2     # 2 link / 1 giây
-DEFAULT_SPAM_MENTION = 5  # >5 tag / 1 tin
+# Cấu hình ngưỡng Anti-Spam
+SPAM_MSG_LIMIT = 5
+SPAM_LINK_LIMIT = 2
+SPAM_MENTION_LIMIT = 5
+
+
